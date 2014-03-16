0e0c2cead948ecfdb79231f35832a2bbbc15476c
Jen Lampton <jen@jeneration.com>
2014-03-15 20:24:20 -0700

    a few more tempates and styles that match our content

--- docroot/sites/all/themes/backdropcms/templates/page.tpl.php
+++ docroot/sites/all/themes/backdropcms/templates/page.tpl.php
@@ -1,16 +1,16 @@
   <div id="page">
 
     <div id="main-wrapper" class="column"><div id="main" class="clearfix">
-      <?php print $messages; ?>
 
       <div id="content"><div class="section">
+        <?php print $messages; ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($title_suffix); ?>
-
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 
         <?php print render($page['content']); ?>
         <?php print $feed_icons; ?>
@@ -18,6 +18,7 @@
 
       <div id="footer"><div class="section">
         <?php print render($page['footer']); ?>
+        <a href="/thank-you">thank you</a>
       </div></div> <!-- /.section, /#footer -->
 
     </div></div> <!-- /#main, /#main-wrapper -->
