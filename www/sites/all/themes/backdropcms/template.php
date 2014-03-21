502f9589f0994b0012835e855670f071a2106eec
Jen Lampton <jen@jeneration.com>
2014-03-20 23:44:30 -0700

    styles for the social media icons

--- www/sites/all/themes/backdropcms/template.php
+++ www/sites/all/themes/backdropcms/template.php
@@ -30,3 +30,21 @@ function backdropcms_preprocess_block(&$variables) {
 /**
  * Theme overrides.
  */
+
+/**
+ * Overrides theme_on_the_web_image().
+ */
+function backdropcms_on_the_web_image($variables) {
+  if ($variables['service'] == 'twitter') {
+    return '<i class="fa fa-twitter-square"></i>';
+  }
+  if ($variables['service'] == 'facebook') {
+    return '<i class="fa fa-facebook-square"></i>';
+  }
+  if ($variables['service'] == 'google') {
+    return '<i class="fa fa-google-plus-square"></i>';
+  }
+  if ($variables['service'] == 'youtube') {
+    return '<i class="fa fa-youtube-square"></i>';
+  }
+}
