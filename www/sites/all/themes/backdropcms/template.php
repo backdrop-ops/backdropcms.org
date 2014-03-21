c339c0a783d52caa93e888c3c3c1fd90810b3794
Jen Lampton <jen@jeneration.com>
2014-03-20 21:56:23 -0700

    add font awesome for menus

--- www/sites/all/themes/backdropcms/template.php
+++ www/sites/all/themes/backdropcms/template.php
@@ -5,8 +5,26 @@
  */
 
 /**
+ * Preprocess functions.
+ */
+
+/**
+ * Prepares variables for page.tpl.php
+ */
+function backdropcms_preprocess_page(&$variables) {
+  foreach($variables['main_menu'] as $key => $item) {
+    $variables['main_menu'][$key]['title'] = t($variables['main_menu'][$key]['title']) . ' <i class="fa fa-book fa-fw"></i>';
+    $variables['main_menu'][$key]['html'] = TRUE;
+  }
+}
+
+/**
  * Prepares variables for block.tpl.php
  */
 function backdropcms_preprocess_block(&$variables) {
 
 }
+
+/**
+ * Theme overrides.
+ */
