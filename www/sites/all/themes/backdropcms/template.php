d851bb07a76398088fe0e005cb5eac130e4f9e70
Jen Lampton <jen@jeneration.com>
2014-03-20 23:06:55 -0700

    icons on the main nav plz

--- www/sites/all/themes/backdropcms/template.php
+++ www/sites/all/themes/backdropcms/template.php
@@ -13,7 +13,7 @@
  */
 function backdropcms_preprocess_page(&$variables) {
   foreach($variables['main_menu'] as $key => $item) {
-    $variables['main_menu'][$key]['title'] = t($variables['main_menu'][$key]['title']) . ' <i class="fa fa-book fa-fw"></i>';
+    $variables['main_menu'][$key]['title'] = t($variables['main_menu'][$key]['title']) . ' <i class="fa fa-chevron-right fa-fw"></i>';
     $variables['main_menu'][$key]['html'] = TRUE;
   }
 }
