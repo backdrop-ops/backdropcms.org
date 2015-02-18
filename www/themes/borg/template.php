65568b489f168541b0b63f23af701b8003a3a8ed
Nathan Haug <nate@lullabot.com>
2015-02-18 01:04:43 -0800

    Adding missing font weights.

--- www/themes/borg/template.php
+++ www/themes/borg/template.php
@@ -13,7 +13,7 @@
  */
 function borg_preprocess_page() {
   // Add the Source Sans Pro font.
-  drupal_add_css('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700', array('type' => 'external'));
+  drupal_add_css('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700', array('type' => 'external'));
   // Add FontAwesome.
   drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array('type' => 'external'));
 }
