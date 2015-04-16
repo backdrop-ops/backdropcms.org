a8a9ad68bb8c63c0b913ccd57c4b292b236db5c4
Jen Lampton <jen@jeneration.com>
2015-04-16 15:52:17 -0700

    Add a content top region to our custom Backdrop layout.

--- www/layouts/double_fixed/layout--double-fixed.tpl.php
+++ www/layouts/double_fixed/layout--double-fixed.tpl.php
@@ -32,14 +32,18 @@
   <div class="l-container">
     <main class="l-content" role="main">
       <div class="l-content-inner">
+        <div class="l-content-top">
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-          <h1 class="title" id="page-title">
-            <?php print $title; ?>
-          </h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+            <h1 class="title" id="page-title">
+              <?php print $title; ?>
+            </h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+
+          <?php print $content['content-top']; ?>
+        </div>
 
         <?php if ($tabs): ?>
           <div class="tabs">
