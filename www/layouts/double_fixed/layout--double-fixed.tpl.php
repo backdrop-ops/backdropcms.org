7c17ef0067aebda5fdb726b9e75c8eef3577cc82
Wes Ruvalcaba <wesruv@gmail.com>
2015-08-13 23:37:26 -0400

    Fixed some theme issues and added all markup and styles necessary for project search
    Added layouts for modules, layouts, and themes, they're all the same... not sure if there's a good way to use one layout?
    Added views config, which is slightly modified from what was given to me
    Added interim styles for project pages, until we get the content spit out so it can be styled like the design
    Added a utility class 'l-content-wrapper' which is used to contain content
    * Normally this would be put on the outer wrapper of a block, but if a block has a bg color that should span the width of the page the class should go on an inner element
    Reworked how the menu flies out of the side, added margin to the body instead of an outer div
    * This was to make sure all content will be in the viewable area (selfishly my krumo output was hiding behind the drawer)
    Changed the way the Backdrop logo is added at mobile to prevent content overlapping
    Fixed misc. bugs that were happening at smaller screensizes by adding extra media query styles that maintained the same appearance at desktop

--- www/layouts/double_fixed/layout--double-fixed.tpl.php
+++ www/layouts/double_fixed/layout--double-fixed.tpl.php
@@ -31,22 +31,23 @@
 
   <div class="l-container">
     <main class="l-content" role="main">
-      <div class="l-content-inner">
         <div class="l-content-top">
-        <a id="main-content"></a>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1 class="title" id="page-title">
-              <?php print $title; ?>
-            </h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
+          <div class="l-content-wrapper">
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+              <h1 class="title" id="page-title">
+                <?php print $title; ?>
+              </h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
 
-          <?php print $content['content-top']; ?>
+            <?php print $content['content-top']; ?>
+          </div>
         </div>
 
         <?php if ($tabs): ?>
-          <div class="tabs">
+          <div class="tabs l-content-wrapper">
             <?php print $tabs; ?>
           </div>
         <?php endif; ?>
