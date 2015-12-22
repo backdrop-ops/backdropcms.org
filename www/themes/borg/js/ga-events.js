2e3dd6078518feb108f9bea138f398a35e147326
Jim Birch <jim@xenomedia.com>
2015-12-22 00:19:10 -0600

    Added Google Analytics Event Tracking in the file ga-events.js, and included it from borg.info.  Need to check clicks on Backdrop and Pantheon Downloads, and submits on Newsletter subscribe, Support form, and Contact form.

---
+++ www/themes/borg/js/ga-events.js
@@ -0,0 +1,32 @@
+  document.write(5 + 6);
+  /**
+   * Google Analytics Event Tracking
+   */
+(function($) {
+  $(document).ready(function() {
+    $('.button.download').on('click', function(){
+      var href = this.href;
+      ga('send','event','Backdrop','Download',document.title, {'hitCallback': function(){document.location = href;}}); return false;
+    });
+
+    $('.button.demo').on('click', function(){
+      var href = this.href;
+      ga('send','event','Backdrop','Pantheon',document.title, {'hitCallback': function(){document.location = href;}}); return false;
+    });
+
+    $('#mc-embedded-subscribe .button').on('click', function(){
+      var href = this.href;
+      ga('send','event','Newsletter','Subscribe',document.title, {'hitCallback': function(){document.location = href;}}); return false;
+    });
+
+    $('#supporter-node-form .button-primary.form-submit').on('click', function(){
+      var href = this.href;
+      ga('send','event','Support','Submit',document.title, {'hitCallback': function(){document.location = href;}}); return false;
+    });
+
+    $('#contact-site-form .button-primary.form-submit').on('click', function(){
+      var href = this.href;
+      ga('send','event','Contact','Submit',document.title, {'hitCallback': function(){document.location = href;}}); return false;
+    });
+  });
+})(jQuery);
