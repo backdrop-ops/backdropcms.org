ea982abf7f43c0e717f7420583fa31d504ee6cee
Jen Lampton <jen@jeneration.com>
2017-04-07 10:08:29 -0700

    Home page fix, more.

--- www/themes/borg/templates/views-view--news--block.tpl.php
+++ www/themes/borg/templates/views-view--news--block.tpl.php
@@ -25,64 +25,32 @@
  * @ingroup views_templates
  */
 ?>
-<div class="<?php print implode(' ', $classes); ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print (is_array($rows)) ? backdrop_render($rows) : $rows; ?>
-      <?php if ($feed_icon): ?>
-        <div class="feed-icon">
-          <?php print $feed_icon; ?>
+<div class="<?php print implode(' ', $classes); ?> container">
+  <div class="row">
+    <div class="view-content-wrapper col-md-8">
+      <h2>Latest News</h2>
+      <?php if ($rows): ?>
+        <div class="view-content">
+          <?php print (is_array($rows)) ? backdrop_render($rows) : $rows; ?>
+          <?php if ($feed_icon): ?>
+            <div class="feed-icon">
+              <?php print $feed_icon; ?>
+            </div>
+          <?php endif; ?>
+        </div>
+      <?php elseif ($empty): ?>
+        <div class="view-empty">
+          <?php print $empty; ?>
         </div>
       <?php endif; ?>
     </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
+
+    <div class="view-footer-wrapper col-md-4">
+      <?php if ($footer): ?>
+        <div class="view-footer">
+          <?php print $footer; ?>
+        </div>
+      <?php endif; ?>
     </div>
-  <?php endif; ?>
-
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
-
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
-
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
-
+  </div>
 </div><?php /* class view */ ?>
