9c1284cc117fd82bf816699a7bbc9e4c852822ee
Jen Lampton <jen@jeneration.com>
2017-12-29 13:13:10 -0800

    Add a new layout for services display. Better contractor layout.

---
+++ www/layouts/burr_flipped/layout--burr-flipped.tpl.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @file
+ * Template for the Moscone Flipped layout.
+ *
+ * Variables:
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: Status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node.)
+ * - $action_links: Array of actions local to the page, such as 'Add menu' on
+ *   the menu administration interface.
+ * - $classes: Array of CSS classes to be added to the layout wrapper.
+ * - $attributes: Array of additional HTML attributes to be added to the layout
+ *     wrapper. Flatten using backdrop_attributes().
+ * - $content: An array of content, each item in the array is keyed to one
+ *   region of the layout. This layout supports the following sections:
+ *   - $content['header']
+ *   - $content['top']
+ *   - $content['content']
+ *   - $content['sidebar']
+ *   - $content['bottom']
+ *   - $content['footer']
+ */
+?>
+<div class="layout--burr-flipped <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <?php if ($content['header']): ?>
+    <header class="l-header" role="banner" aria-label="<?php print t('Site header'); ?>">
+      <div class="l-header-inner container container-fluid">
+        <?php print $content['header']; ?>
+      </div>
+    </header>
+  <?php endif; ?>
+
+  <?php if (!empty($content['top'])): ?>
+  <div class="l-top">
+      <div class="l-top-inner container container-fluid">
+        <?php print $content['top']; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div> <!-- .l-wrapper !-->
+    <div class="l-wrapper-inner container container-fluid">
+
+      <?php if ($messages): ?>
+        <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
+          <?php print $messages; ?>
+        </div>
+      <?php endif; ?>
+
+      <div class="l-page-title">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+
+      <?php if ($tabs): ?>
+        <nav class="tabs" role="tablist" aria-label="<?php print t('Admin content navigation tabs.'); ?>">
+          <?php print $tabs; ?>
+        </nav>
+      <?php endif; ?>
+
+      <?php print $action_links; ?>
+
+      <div class="l-middle row">
+        <main class="l-content col-lg-8" role="main" aria-label="<?php print t('Main content'); ?>">
+          <?php print $content['content']; ?>
+        </main>
+        <div class="l-sidebar l-sidebar-first col-lg-4">
+          <?php print $content['sidebar']; ?>
+        </div>
+      </div><!-- /.l-middle -->
+
+    </div><!-- /.l-wrapper-inner -->
+  </div><!-- /.l-wrapper -->
+
+  <?php if (!empty($content['bottom'])): ?>
+    <div class="l-bottom">
+      <?php print $content['bottom']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($content['footer']): ?>
+    <footer class="l-footer"  role="footer">
+      <div class="l-footer-inner container container-fluid">
+        <div class="row">
+          <?php print $content['footer']; ?>
+        </div>
+      </div>
+    </footer>
+  <?php endif; ?>
+</div><!-- /.layout--moscone-flipped -->
