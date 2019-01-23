e8af871227b5fa7ca7caf4321482d8160e4e879e
quicksketch <nate@quicksketch.org>
2019-01-22 22:06:23 -0800

    Issue #422: Adding docs, styling, and cleanup cron job to sandboxes.

--- www/modules/custom/borg_tugboat/templates/borg-tugboat-demo-done-page.tpl.php
+++ www/modules/custom/borg_tugboat/templates/borg-tugboat-demo-done-page.tpl.php
@@ -13,7 +13,7 @@
     <?php print l($url, $url); ?>
   </p>
   <p class="borg-demo-button">
-    <?php print l(t('Visit your site'), $url, array('attributes' => array('class' => array('button')))); ?>
+    <?php print l(t('Visit your site'), $url, array('attributes' => array('class' => array('button button-large')))); ?>
   </p>
 
   <p class="borg-demo-persist-notice">
@@ -25,5 +25,35 @@
     downloaded it, including running the installer. You'll set an administrator
     account as part of the setup. Note that sending email from a demo sandbox
     is not allowed, so <strong>password recovery will not work</strong>. Be sure
-    to remember your password.</p>
+    to remember your password.
+  </p>
+
+  <p class="borg-demo-time-notice">
+    There is no export functionality in the demo sandbox. <strong>Any work you
+    do will be temporary</strong> and be deleted after <?php print $duration; ?>.
+  </p>
+
+  <figure class="borg-demo-tugboat-information">
+    <a href="https://tugboat.qa">
+    <?php print theme('image', array(
+      'path' => backdrop_get_path('module', 'borg_tugboat') . '/images/tugboat-logo.png',
+      'alt' => 'Tugboat QA Logo',
+      'width' => 800,
+      'height' => 300,
+      'attributes' => array('class' => array('tugboat-logo')),
+    )); ?>
+    </a>
+    <figcaption>
+      Backdrop CMS demo sandboxes are provided by <a href="http://tugboat.qa">Tugboat.qa</a>,
+      a service that can create on-demand site previews for pull requests. To learn more
+      about Tugboat, visit <a href="https://tugboat.qa">https://tugboat.qa</a>.
+    </figcaption>
+  </figure>
+
+  <p class="borg-demo-contribute">
+    Interested in how these sandboxes are built or have ideas for making them
+    better? You can <a href="https://github.com/backdrop-ops/backdropcms.org/tree/master/www/modules/custom/borg_tugboat">view the source code for this functionality</a>
+    in the GitHub repository. File an issue or pull request in the
+    <a href="https://github.com/backdrop-ops/backdropcms.org/issues">BackdropCMS.org issue tracker</a>.
+  </p>
 </div>
