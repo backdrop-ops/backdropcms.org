e9078bfbd0f459d0045f25f441feddbc0aeba4b4
Jen Lampton <jen@jeneration.com>
2019-06-04 17:42:48 -0700

    Refer to demos as 'demo sandboxes' consistently.

--- www/modules/custom/borg_tugboat/templates/borg-tugboat-demo-create-page.tpl.php
+++ www/modules/custom/borg_tugboat/templates/borg-tugboat-demo-create-page.tpl.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Template for the create demo page.
+ * Template file for the create demo page.
  */
 ?>
 
 <div class="borg-demo-create-page">
-  <p class="borg-demo-create-intro">
-    Get started by clicking the button below! Creating a new site may take a few minutes.
-  </p>
+  <h2>See what it's like to use Backdrop CMS.</h2>
+  <p class="borg-demo-create-intro">Use your own, free, demonstration website to take Backdrop CMS for a spin.</p>
 
   <?php print render($form); ?>
+  <p>Creating a new sandbox may take a moment.</p>
 </div>
