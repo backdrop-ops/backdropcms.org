13220a5bd7d808eef1ba803d86de6e781fc46cbe
Jen Lampton <jen@jeneration.com>
2019-06-14 13:13:52 -0700

    Sync live to stage.

--- www/themes/borg/templates/block--menu.tpl.php
+++ www/themes/borg/templates/block--menu.tpl.php
@@ -3,6 +3,12 @@
  * Remove the title from all menu blocks.
  */
 ?>
-<div class="<?php print implode(' ', $classes); ?>">
+<div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h2 class="block-title"><?php print $title; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
   <?php print render($content); ?>
 </div>
