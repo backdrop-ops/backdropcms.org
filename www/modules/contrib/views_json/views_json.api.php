674b508cc44963f1dee0ac5e0926318464c53c4d
stpaultim <tim@politalk.org>
2019-12-14 14:59:15 -0600

    Adding Views JSON Module

---
+++ www/modules/contrib/views_json/views_json.api.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Describe hooks provided by the Views datasource module.
+ */
+
+/**
+ * Alters rendered json row.
+ *
+ * @param $field_output array
+ *   The output rendered by _views_json_render_fields().
+ * @param $view view
+ *   The view that is being rendered.
+ * @param $row stdClass
+ *   Raw data collected by views_plugin_json_style().
+ *
+ * @see _views_json_render_fields().
+ * @see views_plugin_json_style().
+ */
+function hook_views_json_render_row_alter(&$field_output, $view, $row) {
+  if (isset($row->field_entity_reference[0]['raw']['entity'])) {
+    $entity = $row->field_entity_reference[0]['raw']['entity'];
+    // Note: $field_output is modified by reference, it is not returned.
+    $field_output['field_entity_reference']->content = array(
+      'type' => $entity->type,
+      'title' => $entity->title,
+      'entity_id' => $entity->entity_id,
+    );
+  }
+}
