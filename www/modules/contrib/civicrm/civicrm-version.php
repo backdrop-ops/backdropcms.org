9b4ca6a473fd51d67d8629465c5dbdc0038d45d5
Morgan <morgan@palantetech.coop>
2021-04-23 11:01:59 -0400

    CiviCRM 5.36.1

--- www/modules/contrib/civicrm/civicrm-version.php
+++ www/modules/contrib/civicrm/civicrm-version.php
@@ -1,7 +1,7 @@
 <?php
 /** @deprecated */
 function civicrmVersion( ) {
-  return array( 'version'  => '5.36.0',
+  return array( 'version'  => '5.36.1',
                 'cms'      => 'Backdrop',
                 'revision' => '' );
 }
