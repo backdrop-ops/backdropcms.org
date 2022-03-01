ab600430f1aaa5c2b70585710997dccf6203315b
Jen Lampton <jen@jeneration.com>
2022-02-28 19:33:56 -0800

    Add more classes to borg sutro for better offsets.

--- www/layouts/borg_sutro/borg_sutro.php
+++ www/layouts/borg_sutro/borg_sutro.php
@@ -21,15 +21,20 @@ function template_preprocess_layout__borg_sutro(&$variables) {
   }
 
   if ($top_region_count) {
-    $variables['spacer_class'] = 'l-space-below';
     switch ($top_region_count) {
       case 1:
+        $variables['corner_spacer_class'] = 'l-space-below-1';
+        $variables['corner_container_class'] = 'l-cut-cormers-1';
         $variables['top_column_classes'][] = 'col-md-12';
         break;
       case 2:
+        $variables['corner_spacer_class'] = 'l-space-below-2';
+        $variables['corner_container_class'] = 'l-cut-cormers-2';
         $variables['top_column_classes'][] = 'col-md-6';
         break;
       case 3:
+        $variables['corner_spacer_class'] = 'l-space-below-3';
+        $variables['corner_container_class'] = 'l-cut-cormers-3';
         $variables['top_column_classes'][] = 'col-md-4';
         break;
     }
