209c63a1b965fda9bfca2c6df900602dbf0f3f3c
Robert J. Lang <robert@langorigami.com>
2022-11-10 06:31:09 -0800

    Update CiviCRM to version 5.55.1.

--- www/modules/civicrm/vendor/composer/autoload_static.php
+++ www/modules/civicrm/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c
+class ComposerStaticInit979a35efe86d96af85781aa37756132d
 {
     public static $files = array (
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
@@ -738,11 +738,11 @@ class ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInitcfd7e33cc9332dfed87d0b9a6112a22c::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit979a35efe86d96af85781aa37756132d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit979a35efe86d96af85781aa37756132d::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit979a35efe86d96af85781aa37756132d::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInit979a35efe86d96af85781aa37756132d::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInit979a35efe86d96af85781aa37756132d::$classMap;
 
         }, null, ClassLoader::class);
     }
