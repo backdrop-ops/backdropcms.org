0602b88a20a350242f0ffc663c8bfd9e7ce4c428
Robert J. Lang <robert@langorigami.com>
2023-04-03 19:52:26 -0700

    Add webform and webform_civicrm modules to beta branch.

---
+++ www/modules/contrib/webform_civicrm/js/contact_component.js
@@ -0,0 +1,109 @@
+/**
+ * Javascript Module for administering the webform_civicrm contact field.
+ */
+
+var wfCiviContact = (function ($, D) {
+
+  var pub = {};
+
+  pub.init = function (path) {
+    var field = $('#default-contact-id');
+    var cid = field.attr('defaultValue');
+    var ret = null;
+    if (cid) {
+      if (cid == field.attr('data-civicrm-id')) {
+        ret = [{id: cid, name: field.attr('data-civicrm-name')}];
+      }
+      else {
+        // If for some reason the data is not embedded, fetch it from the server
+        $.ajax({
+          url: path,
+          data: {cid: cid, load: 'name'},
+          dataType: 'json',
+          async: false,
+          success: function(data) {
+            if (data) {
+              ret = [{id: cid, name: data}];
+            }
+          }
+        });
+      }
+    }
+    return ret;
+  };
+
+  D.behaviors.webform_civicrmContact = {
+    attach: function (context) {
+      function changeDefault() {
+        var val = $(this).val().replace(/_/g, '-');
+        $('#edit-defaults > div > .form-item', context).not('.form-item-extra-default, .form-item-extra-allow-url-autofill').each(function() {
+          if (val.length && $(this).is('[class*=form-item-extra-default-'+val+']')) {
+            $(this).removeAttr('style');
+          }
+          else {
+            $(this).css('display', 'none');
+            $(':checkbox', this).prop('disabled', true);
+          }
+          if (val == 'custom-ref' && $(this).is('[class*=form-item-extra-default-relationship-to]')) {
+            $(this).removeAttr('style');
+          }
+          if (val == 'case-roles' && ($(this).is('[class*=form-item-extra-default-relationship-to]') || $(this).is('[class*=form-item-extra-default-for-case]'))) {
+            $(this).removeAttr('style');
+          }
+        });
+        if (val === 'auto' || val === 'relationship' || val == 'custom-ref' || val == 'case-roles') {
+          $('.form-item-extra-randomize, .form-item-extra-dupes-allowed')
+            .removeAttr('style')
+            .find(':checkbox')
+            .prop('disabled', false);
+        }
+        $('#edit-extra-default-relationship-to', context).each(changeDefaultRelationTo);
+      }
+      function changeDefaultRelationTo() {
+        var c = $(this).val(),
+          types = $(this).closest('form').data('reltypes')[c],
+          placeholder = types.length ? false : '- ' + Backdrop.ts('No relationship types available for these contact types') + ' -';
+        CRM.utils.setOptions('#edit-extra-default-relationship', types, placeholder);
+        // Provide default to circumvent "required" validation error
+        if ($('#edit-extra-default').val() !== 'relationship' && !types.length && types[0].key === '') {
+          CRM.utils.setOptions('#edit-extra-default-relationship', {key: '-', value: '-'});
+          $('#edit-extra-default-relationship').val('-');
+        }
+      }
+      function changeFiltersRelationTo() {
+        var c = $(this).val(),
+          types = $(this).closest('form').data('reltypes')[c];
+        $('.form-item-extra-filters-relationship-type', context).toggle(!!c);
+        if (c) {
+          CRM.utils.setOptions('#edit-extra-filters-relationship-type', types);
+        }
+      }
+      $('#edit-extra-default', context).once('wf-civi').change(changeDefault).each(changeDefault);
+      $('#edit-extra-default-relationship-to', context).once('wf-civi').change(changeDefaultRelationTo);
+      $('#edit-extra-filters-relationship-contact', context).once('wf-civi').change(changeFiltersRelationTo).each(changeFiltersRelationTo);
+      $('#edit-extra-widget', context).once('wf-civi').change(function() {
+        if ($(this).val() == 'hidden') {
+          $('.form-item-extra-search-prompt', context).css('display', 'none');
+          $('.form-item-extra-show-hidden-contact', context).removeAttr('style');
+        }
+        else {
+          $('.form-item-extra-search-prompt', context).removeAttr('style');
+          $('.form-item-extra-show-hidden-contact', context).css('display', 'none');
+        }
+      }).change();
+
+      $('select[name*=hide_fields]', context).once('wf-civi').change(function() {
+        $(this).parent().nextAll('.form-item').toggle(!!$(this).val());
+      }).change();
+
+      // Warning if enforce permissions is disabled
+      $('#webform-component-edit-form', context).once('wf-civi').submit(function() {
+        if (!$('input[name="extra[filters][check_permissions]"]').is(':checked') && $('input[name="extra[allow_url_autofill]"]').is(':checked')) {
+          return confirm(Backdrop.t('Warning: "Enforce Permissions" is disabled but "Use contact id from URL" is enabled. Anyone with access to this webform will be able to view any contact in the database (who meets the filter criteria) by typing their contact id in the URL.'));
+        }
+      });
+    }
+  };
+
+  return pub;
+})(jQuery, Backdrop);
---
+++ www/modules/contrib/webform_civicrm/js/webform_civicrm_payment.js
@@ -0,0 +1,144 @@
+// Webform payment processing using CiviCRM's jQuery
+cj(function($) {
+  'use strict';
+  var
+    setting = Backdrop.settings.webform_civicrm,
+    $processorFields = $('.civicrm-enabled[name$="civicrm_1_contribution_1_contribution_payment_processor_id]"]');
+
+  $(document).ajaxStart(function() {
+    $('#billing-payment-block').closest('form').block();
+  })
+  .ajaxStop(function() {
+    $('#billing-payment-block').closest('form').unblock();
+  });
+
+  function getPaymentProcessor() {
+    if (!$processorFields.length) {
+      return setting.paymentProcessor;
+    }
+    return $processorFields.filter('select, :checked').val();
+  }
+
+  function loadBillingBlock() {
+    var type = getPaymentProcessor();
+    if (type && type != '0') {
+      $('#billing-payment-block').load(setting.contributionCallback + '&' + setting.processor_id_key + '=' + type, function() {
+        $('#billing-payment-block').trigger('crmLoad').trigger('crmFormLoad');
+        if (setting.billingSubmission) {
+          $.each(setting.billingSubmission, function(key, val) {
+            $('[name="' + key + '"]').val(val);
+          });
+        }
+        // When an express payment button is clicked, skip the billing fields and submit the form with a placeholder
+        var $expressButton = $('input[name$=_upload_express]', '#billing-payment-block');
+        if ($expressButton.length) {
+          $expressButton.removeClass('crm-form-submit').click(function(e) {
+            e.preventDefault();
+            $('input[name=credit_card_number]', '#billing-payment-block').val('express');
+            $(this).closest('form').find('input.webform-submit.button-primary').click();
+          })
+        }
+      });
+    }
+    else {
+      $('#billing-payment-block').html('');
+    }
+  }
+  $processorFields.on('change', function() {
+    setting.billingSubmission || (setting.billingSubmission = {});
+    $('input:visible, select', '#billing-payment-block').each(function() {
+      var name = $(this).attr('name');
+      name && (setting.billingSubmission[name] = $(this).val());
+    });
+    loadBillingBlock();
+  });
+  loadBillingBlock();
+
+  function getTotalAmount() {
+    var totalAmount = 0.0;
+    $('.line-item:visible', '#wf-crm-billing-items').each(function() {
+      totalAmount += parseFloat($(this).data('amount'));
+    });
+    return totalAmount;
+  }
+
+  function tally() {
+    var total = 0;
+    total = getTotalAmount();
+
+    $('td+td', '#wf-crm-billing-total').html(CRM.formatMoney(total));
+    if (total > 0) {
+      $('#billing-payment-block').show();
+    } else {
+      $('#billing-payment-block').hide();
+    }
+  }
+
+  function updateLineItem(item, amount, label) {
+    var $lineItem = $('.line-item.' + item, '#wf-crm-billing-items');
+    if (!$lineItem.length) {
+      var oe = $('#wf-crm-billing-items tbody tr:first').hasClass('odd') ? ' even' : ' odd';
+      $('#wf-crm-billing-items tbody').prepend('<tr class="line-item ' + item + oe + '" data-amount="' + amount + '">' +
+        '<td>' + label + '</td>' +
+        '<td>' + CRM.formatMoney(amount) + '</td>' +
+      '</tr>');
+    }
+    else {
+      var taxPara = 1;
+      var tax = $lineItem.data('tax');
+      if (tax && tax !== '0') {
+        taxPara = 1 + (tax / 100);
+      }
+      $('td+td', $lineItem).html(CRM.formatMoney(amount * taxPara));
+      $lineItem.data('amount', amount * taxPara);
+    }
+    tally();
+  }
+
+  function getFieldAmount(fid) {
+    var amount, total = 0;
+    $('input.civicrm-enabled[name*="' + fid + '"], select.civicrm-enabled[name*="' + fid +'"] option')
+      .filter('option:selected, [type=hidden], [type=number], [type=text], :checked')
+      .each(function() {
+        amount = parseFloat($(this).val());
+        if ($(this).is('.webform-grid-option input')) {
+          var mult = parseFloat(this.name.substring(this.name.lastIndexOf('[')+1, this.name.lastIndexOf(']')));
+          !isNaN(mult) && (amount = amount * mult);
+        }
+        total += isNaN(amount) ? 0 : amount;
+      });
+    return total < 0 ? 0 : total;
+  }
+
+  function calculateLineItemAmount() {
+    var fieldKey = $(this).data('civicrmFieldKey'),
+      amount = getFieldAmount(fieldKey),
+      label = $(this).closest('div.webform-component').find('label').text() || Backdrop.t('Contribution'),
+      lineKey = fieldKey.split('_').slice(0, 4).join('_');
+    updateLineItem(lineKey, amount, label);
+  }
+
+  $('.civicrm-enabled.contribution-line-item:not(".form-radios, .form-checkboxes")')
+    .each(calculateLineItemAmount)
+    .on('change keyup', calculateLineItemAmount)
+    .each(function() {
+      // Also use Backdrop's jQuery to listen to this event, for compatibility with other modules
+      jQuery(this).change(calculateLineItemAmount);
+    });
+
+  tally();
+
+  var payment = {
+    getTotalAmount: function() {
+      return getTotalAmount();
+    }
+  };
+
+  if (typeof CRM.payment === 'undefined') {
+    CRM.payment = payment;
+  }
+  else {
+    $.extend(CRM.payment, payment);
+  }
+
+});
